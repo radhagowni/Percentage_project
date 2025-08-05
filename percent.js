761d2c5c273330fc4675dba00fea45aa86001004
DEEPA RADHA <deeparadha@DEEPAs-MacBook-Air.local>
2025-08-05 11:47:26 +0530

    Initial commit

---
+++ percent.js
@@ -0,0 +1,20 @@
+const Inter=document.getElementById("Inter");
+const SSc=document.getElementById("SSc");
+const Btech=document.getElementById("Btech");
+const countLabel=document.getElementById("countLabel");
+let count=0;
+Inter.onclick=function()
+{
+    count=96.9;
+   countLabel.textContent=count;
+}
+SSc.onclick=function()
+{
+    count=97.5;
+   countLabel.textContent=count;
+}
+Btech.onclick=function()
+{
+    count=88.2;
+   countLabel.textContent=count;
+}
